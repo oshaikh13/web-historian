38b01385b85f3a5897f73b4923d10e1a07fb3308
Hack Reactor Students <accounts@hackreactor.com>
2015-07-23 12:04:48 -0700

    added cron script

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,13 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require('../helpers/archive-helpers.js');
+var fs = require('fs');
+
+
+archive.readListOfUrls(function(data){
+  console.log(data);
+  archive.downloadUrls(data, function(){
+    fs.writeFile(archive.paths.list, '', function(){console.log('done')});
+  });
+  
+})
